60d898f6c31ead2d13929561ffe70d80fe16373f
Jack Grebenc <jgrebenc@konradgroup.com>
2025-06-19 23:32:35 -0400

    updates

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,8 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jackgrebenc.github.io',
   base: '/jg-golf',
+
+  vite: {
+  plugins: [tailwindcss()],
+ },
+
+  integrations: [react()],
 })
