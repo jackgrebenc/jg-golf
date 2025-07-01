b78b398222d3cab5dad89badc91c43838b935661
Jack Grebenc <jgrebenc@konradgroup.com>
2025-07-01 10:59:25 -0400

    updates to website nav

--- astro/ui/components/ui/interactiveNav.tsx
+++ astro/ui/components/ui/interactiveNav.tsx
@@ -1,11 +1,14 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import { useState } from "react";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+
+import { useWindowSize } from "@/lib/useWindowSize";
 
 export type NavItem = {
   title: string;
@@ -17,6 +20,16 @@ interface InteractiveNavProps {
 }
 
 export const InteractiveNav = ({ navItems }: InteractiveNavProps) => {
+  const { isMobile } = useWindowSize();
+
+  return isMobile ? (
+    <HamburgerMenu navItems={navItems} />
+  ) : (
+    <FullMenu navItems={navItems} />
+  );
+};
+
+export const FullMenu = ({ navItems }: InteractiveNavProps) => {
   return (
     <NavigationMenu>
       <NavigationMenuList>
@@ -33,3 +46,27 @@ export const InteractiveNav = ({ navItems }: InteractiveNavProps) => {
     </NavigationMenu>
   );
 };
+
+export default function HamburgerMenu({ navItems }: InteractiveNavProps) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="default" className="md:hidden">
+          <img src="menu.svg" />
+          <span className="sr-only">Toggle menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="bottom" className="px-5 py-5 w-full">
+        <nav className="flex flex-col space-y-10 text-emerald-800 font-bold text-center">
+          {navItems.map((item) => (
+            <a key={item.title} className="text-xl" href={item.href}>
+              {item.title}
+            </a>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+}
